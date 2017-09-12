26427a23e00b1f36d607d1dd4de63eb31b93e099
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-09-12 17:48:17 +0700

    website for showing

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,26 +6,29 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import conciergecoin_artifacts from '../../build/contracts/ConciergeCoin.json'
+import conciergecoin_crowdsale_artifacts from '../../build/contracts/ConciergeCoinCrowdsale.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+// ConciergeCoin is our usable abstraction, which we'll use through the code below.
+var ConciergeCoin = contract(conciergecoin_artifacts);
+var ConciergeCoinCrowdsale = contract(conciergecoin_crowdsale_artifacts);
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
 var accounts;
 var account;
+var conciergeCoinInstance;
 
 window.App = {
   start: function() {
     var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+    // Bootstrap the ConciergeCoin abstraction for Use.
+    ConciergeCoinCrowdsale.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
+    web3.eth.getAccounts(async function(err, accs) {
       if (err != null) {
         alert("There was an error fetching your accounts.");
         return;
@@ -37,9 +40,12 @@ window.App = {
       }
 
       accounts = accs;
-      account = accounts[0];
+      account = accounts[0];      
 
-      self.refreshBalance();
+      var crowdsale = await ConciergeCoinCrowdsale.deployed();
+      var tokenAddress = await crowdsale.token();
+      conciergeCoinInstance = ConciergeCoin.at(tokenAddress);                
+
     });
   },
 
@@ -48,23 +54,20 @@ window.App = {
     status.innerHTML = message;
   },
 
-  refreshBalance: function() {
+  refreshBalance: async function() {
     var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
+    try{
+      var receiver = document.getElementById("receiver").value;
+      var balance = conciergeCoinInstance.balanceOf(receiver);        
       var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
+      balance_element.innerHTML = balance.toString(10);
+    } catch(e) {
       console.log(e);
       self.setStatus("Error getting balance; see log.");
-    });
+    }
   },
 
-  sendCoin: function() {
+  sendCoin: async function() {
     var self = this;
 
     var amount = parseInt(document.getElementById("amount").value);
@@ -72,30 +75,30 @@ window.App = {
 
     this.setStatus("Initiating transaction... (please wait)");
 
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
+    try{
+      var crowdsale = await ConciergeCoinCrowdsale.deployed();
+      web3.personal.unlockAccount(receiver, "123456")
+      crowdsale.sendTransaction({ from: receiver, value: web3.toWei(amount, "ether")})      
+    
       self.setStatus("Transaction complete!");
       self.refreshBalance();
-    }).catch(function(e) {
+    } catch(e) {
       console.log(e);
       self.setStatus("Error sending coin; see log.");
-    });
+    }
   }
 };
 
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 ConciergeCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
+    window.web3 = new Web3(web3.currentProvider);    
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));    
   }
 
   App.start();
