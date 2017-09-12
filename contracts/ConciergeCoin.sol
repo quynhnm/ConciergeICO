63b8170d155e8407ec6fa5fbb94a21d85d8148d9
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-09-12 11:42:21 +0700

    update coin name

--- contracts/ConciergeCoin.sol
+++ contracts/ConciergeCoin.sol
@@ -4,6 +4,6 @@ import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 
 contract ConciergeCoin is MintableToken {
   string public name = "CONCIERGE COIN";
-  string public symbol = "CEC";
+  string public symbol = "CC1";
   uint256 public decimals = 18;
 }
