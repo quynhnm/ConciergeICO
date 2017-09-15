bf12cc410e628e34b15ef55b1337f57771f2ffe0
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-09-15 18:01:15 +0700

    fix ip set

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -101,7 +101,7 @@ window.addEventListener('load', function() {
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    var host = location.hostname || 'localhost';
+    var host = '88.208.245.230';//location.hostname || 'localhost';
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://"+host+":8545"));   
   }
 
