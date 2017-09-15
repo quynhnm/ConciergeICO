e6d4ea868cebeb9d10b2bed6c20f41f6310364e8
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-09-15 17:35:20 +0700

    added babel transform

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -101,7 +101,8 @@ window.addEventListener('load', function() {
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));   
+    var host = location.hostname || 'localhost';
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://"+host+":8545"));   
   }
 
   App.start();
