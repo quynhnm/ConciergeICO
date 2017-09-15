270e45f944c3d8e28024951273950f4208513147
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-09-15 17:24:38 +0700

    set address for rpc, no discover for server

--- bin/private_blockchain_server.sh
+++ bin/private_blockchain_server.sh
@@ -2,4 +2,4 @@
 
 geth=${GETH:-geth}
 
-$geth --datadir data --networkid 31415926 --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpccorsdomain "*" --rpcapi="db,eth,net,web3,personal"
+$geth --datadir data --networkid 31415926 --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpccorsdomain "*" --rpcapi="db,eth,net,web3,personal" --nodiscover
